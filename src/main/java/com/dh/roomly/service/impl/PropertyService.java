a2ff8ce19fe9ca9f09518d396d492483929a06b2
jesus Garcia <jesus.garcia2611@hotmail.com>
2024-11-04 08:08:11 -0500

    nada

--- src/main/java/com/dh/roomly/service/impl/PropertyService.java
+++ src/main/java/com/dh/roomly/service/impl/PropertyService.java
@@ -14,6 +14,7 @@ import com.dh.roomly.repository.specification.PropertySpecification;
 import com.dh.roomly.service.IFileService;
 import com.dh.roomly.service.IPropertyService;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,15 +28,12 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class PropertyService implements IPropertyService {
 
-    @Autowired
-    IPropertyRepository iPropertyRepository;
-    @Autowired
-    IFileRepository fileRepository;
-    @Autowired
-    IFileService fileService;
-    
+    private final IPropertyRepository iPropertyRepository;
+    private final IFileRepository fileRepository;
+    private final IFileService fileService;
 
     @Override
     public PropertyDTO findById(Long id) {
@@ -78,7 +76,7 @@ public class PropertyService implements IPropertyService {
         List<PropertyEntity> properties = iPropertyRepository.findAll();
         return properties.stream()
                 .map(property -> (PropertyDTO) MappingDTO.convertToDto(property, new PropertyDTO()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     private PropertyEntity findPropertyEntityById(Long id){
