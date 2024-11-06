62588c11e303b506034653d093063e56720b3400
Francortiz-137 <francortiz.c@gmail.com>
2024-11-05 21:33:05 -0300

    feat Added Create Category feature

--- src/main/java/com/dh/roomly/controller/CategoryController.java
+++ src/main/java/com/dh/roomly/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.dh.roomly.controller;
 
+import com.dh.roomly.dto.impl.CategoryDTOInput;
 import com.dh.roomly.dto.impl.CategoryDTOOutput;
 import com.dh.roomly.service.ICategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +25,11 @@ public class CategoryController {
     @GetMapping("/all")
     public ResponseEntity<List<CategoryDTOOutput>> getAllCategories() {
         return ResponseEntity.ok(categoryService.findAllCategories());
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<CategoryDTOOutput> createCategory(@RequestBody CategoryDTOInput categoryDTOInput) {
+        CategoryDTOOutput categoryDTOOutput = categoryService.createCategory(categoryDTOInput);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryDTOOutput);
     }
 }
