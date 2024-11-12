89e6124a4d112b9d98817403393331f6df2880cb
Alberto Carrera <acarrera_l@hotmail.com>
2024-11-12 18:55:38 -0500

    Add categories id to product filters

--- src/main/java/com/dh/roomly/dto/filter/PropertyFilterDTO.java
+++ src/main/java/com/dh/roomly/dto/filter/PropertyFilterDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
@@ -28,4 +29,5 @@ public class PropertyFilterDTO{
     private Short maxNumBathrooms;
     private Long ownerId;
     private Short categoryId;
+    private List<Short> categoryIds;
 }
