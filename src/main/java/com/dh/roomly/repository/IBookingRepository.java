199f75fe27b0eedffe8dfb54fc079292b842785f
Francortiz-137 <francortiz.c@gmail.com>
2024-11-29 17:01:05 -0300

    fixed validations missing

--- src/main/java/com/dh/roomly/repository/IBookingRepository.java
+++ src/main/java/com/dh/roomly/repository/IBookingRepository.java
@@ -1,12 +1,17 @@
 package com.dh.roomly.repository;
 
 import com.dh.roomly.entity.BookingEntity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IBookingRepository extends JpaRepository<BookingEntity,Long> {
     List<BookingEntity> findByUserId(Long id);
+    List<BookingEntity> findByUserIdAndPropertyId(Long userId, Long propertyId);
 }
